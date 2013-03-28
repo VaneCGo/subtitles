b10222469eab3a2700d6cf185c844fa984cd646a
Vanessa <vane.cantero.g@gmail.com>
2013-03-28 15:52:23 +0100

    Update the program and README

---
+++ README.md
@@ -0,0 +1,9 @@
+Under construction.
+
+The goal of this program is to improve my python skills. Don't hesitate to make a suggestion to make it better (my skills and the program, of course).
+
+First version
+
+- The program only downloads the subtitles in english.
+
+- The program returns a url where I suppose the subtitle is, but I have to deal with a handler in the browser so the subtitle can be download correctly. I haven't found the way to do it. Any sugestion?
--- prog.py
+++ prog.py
@@ -1,31 +1,47 @@
-#You can save the season that you actualy are seeing and
-#then the program list the chapters. You choose one and then its being downloaded.
-
-#class NombreClase():
-
-#	def __init__(self):
-	
 import urllib2
 import re
 
-def getUrl(name):
-	r = urllib2.urlopen("http://www.subtitulos.es/series")
-	page_code = r.read()
+
+def getUrl(name,page_code):
 	match = re.compile(r'/show/[0-9]+\">%s</a>' % name,re.IGNORECASE)
 	m = match.search(page_code) #search in text
 	if m:
-		pal = page_code[m.start():m.end()]
-		quote = pal.index('"')
-		return pal[:quote] 
+		word = page_code[m.start()+6:m.end()] #only the number
+		quote = word.index('"')
+		close_href = word.find('</a>',quote)
+		name = word[quote+2:close_href]
+		return word[:quote], name 
+	else:
+		return None, None
+
+def getUrlSubtitle(s_season, s_chapter, page_code):
+	form = s_season+"x"+s_chapter
+	match = re.compile(r'/%s' % form, re.IGNORECASE)
+	m = match.search(page_code)
+
+	if m:
+		start_url = page_code.find('http://www.subtitulos.es/updated/1', m.end())
+		end_url = page_code.find('"', start_url)
+		subtitle = page_code[start_url:end_url]
+		return subtitle
 	else:
 		return None
 
-def main():
-	s_name = raw_input("Ingresa el nombre de tu serie: ")
-	url = getUrl(s_name)
-	if url:
-		print "url:",url,"name:",s_name
 
+def main():
+	r = urllib2.urlopen("http://www.subtitulos.es/series")
+	page_code = r.read()
+	s_name = raw_input("Ingresa el nombre de tu serie: ")
+	url, name = getUrl(s_name,page_code)
+
+	s_season = raw_input("Ingresa la temporada: ") #1,2,3,4
+	s_chapter = raw_input("Ingresa el capitulo de la temporada: ") # 01,02...10,11...
+
+	url_req = "http://www.subtitulos.es/ajax_loadShow.php?show="+url+"&season="+s_season
+	q = urllib2.urlopen(url_req)
+	page_code = q.read()
+	url_subtitle = getUrlSubtitle(s_season, s_chapter, page_code)
+	print "URL SUBTITLE ", url_subtitle
    
 main()
 
